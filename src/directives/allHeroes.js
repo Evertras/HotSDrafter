f3bfae872d6028e95ce4d654c2e623c693ffa9dd
evertras <bfullj@gmail.com>
2015-05-20 14:40:48 -0400

    Whoops, extraneous parameter

--- src/directives/allHeroes.js
+++ src/directives/allHeroes.js
@@ -1,7 +1,7 @@
 (function() {
 	var app = angular.module("allHeroes", ["heroDataService"]);
 
-	app.directive("allHeroes", function(heroDataService) {
+	app.directive("allHeroes", function() {
 		return {
 			templateUrl: "templates/all-heroes.html",
 			controller: ['heroDataService', '$scope', function(heroDataService, $scope) {
